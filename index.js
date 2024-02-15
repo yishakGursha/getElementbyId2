d92f943b286f323f7f4ff86812135e2603f686d3
yiso21 <isaacgursha569@gmail.com>
2024-02-15 16:17:53 +0300

    to check

---
+++ index.js
@@ -0,0 +1,8 @@
+// Selecting an element by its ID
+const elementById = document.getElementById("myElementId");
+
+// Modifying the element's text content
+elementById.textContent = "JavaScript modified the content!";
+
+// Modifying the element's style
+elementById.style.backgroundColor = "yellow";
